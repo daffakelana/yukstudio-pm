94a510ba63c5d73e4274818c8bf54cc0a40e9260
daffakelana <daffadarakelana@student.upi.edu>
2025-11-19 20:34:19 +0700

    df-feat : sudahlah

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,7 @@ use Filament\Pages\BasePage as Page;
 use Filament\Resources\Resource;
 use Filament\Widgets\Widget;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\URL; // <-- TAMBAHKAN IMPORT INI
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,11 +27,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        /**
+         * =========================
+         * FIX Mixed Content Railway
+         * =========================
+         */
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        /**
+         * Livewire Component Registration
+         */
         Livewire::component('edit-comment-modal', EditCommentModal::class);
+
+        /**
+         * FilamentShield Permission Key Builder
+         */
         FilamentShield::buildPermissionKeyUsing(
             function (string $entity, string $affix, string $subject, string $case, string $separator) {
-                return match(true) {
-                    # if `configurePermissionIdentifierUsing()` was used previously, then this needs to be adjusted accordingly
+                return match (true) {
                     is_subclass_of($entity, Resource::class) => Str::of($affix)
                         ->snake()
                         ->append('_')
@@ -43,13 +59,16 @@ class AppServiceProvider extends ServiceProvider
                                 ->replace('_', '::')
                         )
                         ->toString(),
+
                     is_subclass_of($entity, Page::class) => Str::of('page_')
                         ->append(class_basename($entity))
                         ->toString(),
+
                     is_subclass_of($entity, Widget::class) => Str::of('widget_')
                         ->append(class_basename($entity))
-                        ->toString()
-                    };
-            });
+                        ->toString(),
+                };
+            }
+        );
     }
 }
